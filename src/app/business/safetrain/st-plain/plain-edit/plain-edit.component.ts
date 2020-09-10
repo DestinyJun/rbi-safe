da6ca51f3d8669eb9a7f1281ae9123da3f8f7118
AssassinMike <1787028816@qq.com>
2020-09-10 10:26:00 +0800

    操作日志：
       （1）第三轮bug修改: 修改新增计划时上个计划缓存数据对下个新增计划数据的影响
        时间: 2020/9/7 11:52
    
        -------安全生产平台-------

--- src/app/business/safetrain/st-plain/plain-edit/plain-edit.component.ts
+++ src/app/business/safetrain/st-plain/plain-edit/plain-edit.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MenuItem} from 'primeng/api';
+import {LocalStorageService} from "../../../../common/services/local-storage.service";
 
 @Component({
   selector: 'app-plain-edit',
@@ -9,10 +10,19 @@ import {MenuItem} from 'primeng/api';
 export class PlainEditComponent implements OnInit {
   public items: MenuItem[];
   public activeIndex = 0;
-  constructor() {
+  constructor(
+    private localSrv: LocalStorageService,
+  ) {
   }
 
   ngOnInit() {
+    // 保证数据的有效性
+    // 清空本地缓存
+    this.localSrv.remove('safeTrainingNeeds');
+    this.localSrv.remove('safeDataPlanList');
+    this.localSrv.remove('safeTestPaper');
+    this.localSrv.remove('safeTestQuestionsList');
+
     this.items = [
       {
       label: '培训计划基础设置',
