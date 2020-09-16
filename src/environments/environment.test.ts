2b404aad6f7f0c5d96663205054a587a57b288cf
DestinyJun <429771890@qq.com>
2020-09-16 10:24:56 +0800

    操作日志： （1）矿山项目开始 ------ 2020-09-15 10:24 ------

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -6,8 +6,6 @@
  */
 export const environment = {
   production: true,
-  // url_safe: 'http://120.77.171.73:8090/security-platform',
-  url_safe: 'http://139.9.153.27:8090/security-platform',
-  // url_safe: 'http://127.0.0.1:8090/security-platform',
+  url_safe: 'http://139.9.153.27:8091/security-mine',
 };
 
